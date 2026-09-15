f337f9ec8d91590b461e23c9cb37c40847dd9605
Камиль Ганиев <kg733904@gmail.com>
2026-09-15 08:44:34 +0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PR1_Kamil_IS141
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
